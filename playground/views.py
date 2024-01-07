a75f90fe96f2d340736f4d3a81a1660bf72ed6bc
Dafa2610 <dafaaryanugraha@gmail.com>
2024-01-07 22:16:21 +0700

    Mengubah Views dan cara menampilkan data

--- playground/views.py
+++ playground/views.py
@@ -9,7 +9,11 @@ from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
 import csv
 import base64
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from flask import Flask, jsonify
+from django.core.management.base import BaseCommand
+from firebase_admin import db
 
 
  
@@ -21,31 +25,25 @@ firebase_admin.initialize_app(cred, {
 })
 
 def get_data_from_firebase(request):
-    # Referensi Firebase
+    # Referensi Firebase 
+
     ref = db.reference('data')  # Ganti dengan path data di Firebase
 
     # Ambil data dari Firebase
     firebase_data = ref.get()
     # Inisialisasi list Django
-    data_list = []
-
-    # with open('data.csv', 'w', newline='') as csvfile:
-    #     fieldnames = ['jmlh', 'lat', 'lng']
-    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    #     writer.writeheader()
-
-    #     # Looping melalui anak-anak (trsk1, trsk2, trsk3) dan menulis data ke dalam file CSV
-    #     for key, value in firebase_data.items():
-    #         row = {
-    #             'jmlh': value.get('jmlh', ''),
-    #             'lat': value.get('lat', ''),
-    #             'lng': value.get('lng', '')
-    #         }
-    #         writer.writerow(row)
-    
     data_trsk = pd.read_csv('data.csv', sep=',')
 
     x = olahdata(data_trsk)
+    # Pilih parameter DBSCAN
+    eps = 0.002  # Sesuaikan nilai ini sesuai kebutuhan Anda
+    min_samples = 5  # Sesuaikan nilai ini sesuai kebutuhan Anda
+
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+    x['cluster'] = dbscan.fit_predict(x[['lat', 'lng']])
+
+
+    # Visualisasi hasil clustering
     # Pilih parameter DBSCAN
     eps = 0.003  # Sesuaikan nilai ini sesuai kebutuhan Anda
     min_samples = 5  # Sesuaikan nilai ini sesuai kebutuhan Anda
@@ -53,19 +51,59 @@ def get_data_from_firebase(request):
     dbscan = DBSCAN(eps=eps, min_samples=min_samples)
     x['cluster'] = dbscan.fit_predict(x[['lat', 'lng']])
 
+    #menghapus kolom geometry
+    x = x.drop(columns=['geometry'])
+
     # Visualisasi hasil clustering
-    plt.figure(figsize=(8, 6))
+    # plt.figure(figsize=(8, 6))
+
     plt.scatter(x['lng'], x['lat'], c=x['cluster'], cmap='viridis', s=5)
     plt.title('DBSCAN Clustering')
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
-    plt.title('Hasil Clustering dengan DBSCAN')
-    plt.colorbar()
-    plt.grid(True)
+    # plt.show()
 
     plt.savefig('Datamining.png')
 
+    selected_columns = x[["jmlh", "lat", "lng", "timestamp", "cluster"]]
+    selected_columns.to_csv("data_selected.csv", index=False)
+
     print(x)
+    
+    for index, row in x.iterrows():
+        data = {
+            'jmlh': int(row['jmlh']),
+            'lat': float(row['lat']),
+            'lng': float(row['lng']),
+            'timestamp': int(row['timestamp']),
+            'cluster': int(row['cluster'])
+        }
+        
+    
+        # Mengirim data ke Firebase
+        # ref.push().set(data)
+
+    # Baca file CSV dan masukkan data ke Firebase Realtime Database
+    # with open('data_selected.csv', newline='') as csvfile:
+    #     csv_reader = csv.DictReader(csvfile)
+    #     for row in csv_reader:
+    #         data = {
+    #             'jmlh': int(row['jmlh']),
+    #             'lat': float(row['lat']),
+    #             'lng': float(row['lng']),
+    #             'timestamp': int(row['timestamp']),
+    #             'cluster': int(row['cluster'])
+    #         }
+    #         # ref.push().set(data)
+    # # print(x)
+
+    # Buka file CSV dalam mode append ('a') agar Anda dapat menambahkan data baru
+    # with open('data_selected.csv', mode='a', newline='') as file:
+    #     writer = csv.writer(file)
+    #     # Tulis data baru ke dalam file CSV
+    #     for row in x:
+    #         writer.writerow(row)
+    #         print(row)
     # Ubah data dari Firebase ke dalam list Django
 
-    return render(request, 'hello.html', {'data_list': firebase_data})
+    return render(request, 'hello.html', {'data_list': selected_columns})
--- tambahdata.py
+++ tambahdata.py
@@ -1,17 +1,35 @@
 import random
 import csv
+from datetime import datetime, timedelta
+
+def generate_random_timestamp_within_last_30_days():
+    # Menghitung tanggal sekarang
+    end_date = datetime.now()
+    
+    # Mendapatkan tanggal 30 hari yang lalu
+    start_date = end_date - timedelta(days=30)
+    
+    # Mengonversi tanggal menjadi detik
+    start_timestamp = int(start_date.timestamp())
+    end_timestamp = int(end_date.timestamp())
+
+    # Menghasilkan timestamp secara acak dalam rentang waktu
+    random_timestamp = random.randint(start_timestamp, end_timestamp)
+    
+    return random_timestamp
 
 # Buat file CSV untuk menyimpan data
 with open('data.csv', 'w', newline='') as csvfile:
-    fieldnames = ['jmlh', 'lat', 'lng']
+    fieldnames = ['jmlh', 'lat', 'lng','timestamp']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
     # Buat 1000 data acak dalam format yang diinginkan
-    for _ in range(500):
+    for _ in range(1000):
         jmlh = random.randint(10000, 100000)
         lat = round(random.uniform(-0.06, -0.01), 6)
         lng = round(random.uniform(109.3, 109.4), 6)
+        timestamp = round(generate_random_timestamp_within_last_30_days())
 
-        row = {'jmlh': jmlh, 'lat': lat, 'lng': lng}
+        row = {'jmlh': jmlh, 'lat': lat, 'lng': lng, 'timestamp': timestamp}
         writer.writerow(row)
